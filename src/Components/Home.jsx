9ddc1532371b626be3fe7a2ad2dda655af6743da
Raihan Mahmud <raihan_uits@outlook.com>
2023-05-18 18:34:31 +0600

    landing feature style

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
             <div className="header-box">
                 <section className='header-section'>
                     <h1>We design and build better chairs, for a better life</h1>
-                    <p class="header-text">
+                    <p className="header-text">
                         In a small shop in the heart of Lisbon, we spend our days
                         relentlessly perfecting the chair. The result is a perfect blend of
                         beauty and comfort, that will have a lasting impact on your health.
@@ -16,24 +16,24 @@ export const Home = () => {
                 </div>
             </div>
             <div>
-                <section className="">
+                <section className="feature-box">
                     <h2>What makes our chairs special</h2>
                     <div className="feature">
-                        <div>
+                        <div className="item">
                             <p><strong>Science meets design</strong></p>
                             <p>
                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
                                 similique adipisci praesentium.
                             </p>
                         </div>
-                        <div>
+                        <div className="item">
                             <p><strong>Maximal comfort</strong></p>
                             <p>
                                 Reprehenderit optio placeat quasi excepturi architecto, explicabo
                                 facilis perspiciatis error maxime magnam.
                             </p>
                         </div>
-                        <div>
+                        <div className="item">
                             <p>
                                 <strong>Ethical and sustainable</strong>
                             </p>
