879cfb0b8eb01217839f6d09e3c8fd308edb9d4a
Raihan Mahmud <raihan_uits@outlook.com>
2023-05-18 12:27:40 +0600

    component tree

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
+import { MainComponent } from "./Components/MainComponent"
 export const App = () => {
   return (
     <>
+    <BrowserRouter>
+      <MainComponent />
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer"
+import { Header } from "./Header"
+
+
+export const MainComponent = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
