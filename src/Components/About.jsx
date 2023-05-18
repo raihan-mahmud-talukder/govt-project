5b74e2e331cff6257df037ccf4836b1f3e9c54fd
Raihan Mahmud <raihan_uits@outlook.com>
2023-05-18 17:23:06 +0600

    landing header

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,8 @@
 export const About = () => {
     document.title = 'About'
     return (
-        <>
+        <main>
             About
-        </>
+        </main>
     )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,20 @@
 export const Home = () => {
     return (
-        <>
-            Home
-        </>
+        <main>
+            <div className="header-box">
+                <section className='header-section'>
+                    <h1>We design and build better chairs, for a better life</h1>
+                    <p class="header-text">
+                        In a small shop in the heart of Lisbon, we spend our days
+                        relentlessly perfecting the chair. The result is a perfect blend of
+                        beauty and comfort, that will have a lasting impact on your health.
+                    </p>
+                    <a className="btn btn--big" href="#">Shop chairs</a>
+                </section>
+                <div className='header-image'>
+                    <img src="images/hero.jpg" alt="Photo" />
+                </div>
+            </div>
+        </main>
     )
 }
