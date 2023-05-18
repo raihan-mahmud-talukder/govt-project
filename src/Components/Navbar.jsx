0481aa3fc2631ce3400fecbea94f9b24740f4fa5
Raihan Mahmud <raihan_uits@outlook.com>
2023-05-18 12:40:51 +0600

    adding router

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,24 @@
+import { Link, Route, Routes } from "react-router-dom"
+import { Home } from "./Home"
+import { About } from "./About"
+import { Services } from "./Services"
+import { Contact } from "./Contact"
+
 export const Navbar = () => {
     return (
         <>
-            Navbar
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/services'>Services</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+            <Routes>
+                <Route path="/" element = {<Home />} />
+                <Route path="/about" element = {<About />} />
+                <Route path="/services" element = {<Services />} />
+                <Route path="/contact" element = {<Contact />} />
+            </Routes>
         </>
     )
 }
